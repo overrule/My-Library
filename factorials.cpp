ca13b4d9b18a273f8855309ec7fe412b68d518b5
overrule <42273932+overrule@users.noreply.github.com>
2020-10-19 19:14:34 +0530

    Create factorials.cpp

---
+++ factorials.cpp
@@ -0,0 +1,31 @@
+ll modpower(ll base, ll power, ll mod=p){
+    ll ans =1;
+    base%=mod;
+    while(power){
+        if(power&1){
+            ans*=base;
+            ans%=mod;
+        }
+        base*=base;
+        base%=mod;
+        power>>=1;
+    }
+    return ans;
+}
+vector<ll> fact;
+vector<ll> invfact;
+void computefactorial(int n){
+    ++n;
+    fact.resize(n);
+    invfact.resize(n);
+    fact[0]=1;
+    for(int i=1;i<n;i++){
+        fact[i]=i*fact[i-1];
+        fact[i]%=p;
+    }
+    invfact[n-1]=modpower(fact[n-1],p-2);
+    for(int i=n-2;i>=0;i--){
+        invfact[i]=(i+1)*invfact[i+1];
+        invfact[i]%=p;
+    }
+}
