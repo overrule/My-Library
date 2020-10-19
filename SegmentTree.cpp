1aab90fad73cd2e3b6deb36246558a11d321d823
overrule <42273932+overrule@users.noreply.github.com>
2020-10-19 19:15:45 +0530

    Create SegmentTree.cpp

---
+++ SegmentTree.cpp
@@ -0,0 +1,56 @@
+template<typename T>
+struct segment_tree {
+    int n;
+    vector<T> segtree;
+    function<T(const T&,const T&)> join;
+    T base = T();
+    segment_tree(int n, function<T(const T&,const T&)> join, T base = T()) : n(n), join(join), base(base){
+        segtree.resize(n << 1, base);
+    }
+    void build() {
+        for (int i = n - 1; i > 0; --i) {
+            segtree[i] = join(segtree[(i << 1)], segtree[(i << 1) | 1]);
+        }
+    }
+    segment_tree(vector<T> seq, function<T(const T&,const T&)> join, T base = T()) : n(seq.size()), join(join), base(base){
+        segtree.resize(n << 1, base);
+        for (int i = 0; i < n; i++) {
+            segtree[n + i] = seq[i];
+        }
+        build();
+    }
+    void calc(int pos){
+        pos >>= 1;
+        while (pos) {
+            segtree[pos] = join(segtree[pos << 1], segtree[(pos << 1) | 1]);
+            pos >>= 1;
+        }
+    }
+    void set(int pos, T val){
+        pos+=n;
+        segtree[pos] = val;
+        calc(pos);
+    }
+    void increment(int pos, T val){
+        pos+=n;
+        segtree[pos] = join(segtree[pos], val);
+        calc(pos);
+    }
+    T query(int l, int r) {
+        T ansl = base;
+        T ansr = base;
+        l += n;
+        r += n + 1;
+        while (l < r) {
+            if (l & 1) {
+                ansl = join(ansl, segtree[l++]);
+            }
+            if (r & 1) {
+                ansr = join(segtree[--r], ansr);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        return join(ansl, ansr);
+    }
+};
