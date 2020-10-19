ab8d5678ebc4d969867b6f8b103dff434da3d9b1
overrule <42273932+overrule@users.noreply.github.com>
2020-10-19 19:15:03 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,33 @@
+struct DSU{
+    vector<int> parent;
+    vector<int> sizof;
+    int n;
+    DSU(int N){
+        n=N;
+        parent.resize(n);
+        sizof.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            sizof[i]=1;
+        }
+    }
+    int get_size(int x){
+        return sizof[find_set(x)];
+    }
+    int find_set(int v){
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+    void join_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (sizof[a] < sizof[b]){
+                swap(a, b);
+            }
+            parent[b] = a;
+            sizof[a] += sizof[b];
+        }
+    }
+};
